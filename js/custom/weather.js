a6ce3e3a0e918c8b8b45dbb1b12b6d58e2234f79
huran <cs147258@vip.qq.com>
2021-12-16 17:31:25 +0800

    Site updated: 2021-12-16 17:31:25

---
+++ js/custom/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "01234",
+        "background": "5",
+        "tmpColor": "4A86E8",
+        "tmpSize": "16",
+        "cityColor": "FF9900",
+        "citySize": "16",
+        "aqiColor": "4A86E8",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 1px 10px 1px",
+        "shadow": "0",
+        "language": "auto",
+        "borderRadius": "15",
+        "fixed": "true",
+        "vertical": "top",
+        "horizontal": "left",
+        "left": "200",
+        "top": "10",
+        "key": "d9f6f25ac3e14e1da59d53cc0061bd1b"
+    }
+}
